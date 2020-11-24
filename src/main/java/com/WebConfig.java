60d2d95651669d255ed98d400414e5e748ea8605
youngG209 <young209@mcocoa.com>
2020-11-24 17:14:19 +0900

    Step 1 - Spring MVC 연동

--- src/main/java/com/WebConfig.java
+++ src/main/java/com/WebConfig.java
@@ -2,8 +2,9 @@ package com;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 @Configuration
 @ComponentScan
-public class AppConfig {
+public class WebConfig {
 }
