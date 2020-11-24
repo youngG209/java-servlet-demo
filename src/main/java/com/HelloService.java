1bc7c6ebff0aa2d95652779d34042a7ebb45d822
youngG209 <young209@mcocoa.com>
2020-11-24 16:53:23 +0900

    Step 1 - Spring IoC 컨테이너 연동 1

---
+++ src/main/java/com/HelloService.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getName() {
+        return "YoungG209";
+    }
+}
--- src/main/java/com/HelloServlet.java
+++ src/main/java/com/HelloServlet.java
@@ -1,5 +1,8 @@
 package com;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,20 +19,25 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         System.out.println("doGet");
+
         response.getWriter().println("<html>");
         response.getWriter().println("<head>");
         response.getWriter().println("<body>");
         response.getWriter().println("<h1>Hello, "
-                + getName()
+                + getNameByService()
                 + "</h1>");
         response.getWriter().println("</body>");
         response.getWriter().println("</head>");
         response.getWriter().println("</html>");
     }
 
-    private Object getName() {
-        return getServletContext().getAttribute("name");
+    private String getNameByService() {
+        ApplicationContext context = (ApplicationContext) getServletContext()
+                .getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        HelloService helloService = context.getBean(HelloService.class);
+        return helloService.getName();
     }
 
     @Override
